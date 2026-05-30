83d4bfda01d9e24cd65c0e716a1aea81d6a44f99
Your Name <sureshmel78@gmail.com>
2026-05-30 20:23:52 +0530

    Remove obsolete files from merged repository

--- modelsCargo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CargoSchema = new mongoose.Schema({
-commodity:String,
-loadPort:String,
-qty:Number
-});
-
-module.exports = mongoose.model("Cargo",CargoSchema);
